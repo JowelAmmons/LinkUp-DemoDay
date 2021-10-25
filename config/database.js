0232c87db05f429ac19d6b707d2acf62153a5713
jowel.ammons@gmail.com <89389822+JowelAmmons@users.noreply.github.com>
2021-10-24 21:11:16 -0400

    passionate project in MVP state and being worked on to be presented as an idea to potentially receive sponsorship

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://morphius:Checkmate5248!@cluster0.2dgsk.mongodb.net/LinkUpLogin?retryWrites=true&w=majority', 
+    'dbName': 'Login'
+};
+
