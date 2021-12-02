d10f197f7b430e57f3836cf878f8ccda22e146bb
jowel.ammons@gmail.com <89389822+JowelAmmons@users.noreply.github.com>
2021-12-02 15:11:35 -0500

    mvp changes final

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,20 @@
+const { ObjectId } = require("bson");
+const { result, find } = require("lodash");
+
 module.exports = function(app, passport, db, multer) {
+
+
+
+ // Image Upload Code =========================================================================
+ const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images/uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + ".png")
+  }
+});
+const upload = multer({storage: storage}); 
 
 // normal routes ===============================================================
 
@@ -24,53 +40,119 @@ module.exports = function(app, passport, db, multer) {
         res.redirect('/');
     });
 
-    //Learn *Free*
+    //Learn 
     app.get('/learn', function(req, res) {
       res.render('learn.ejs');
     })
 
     // FORUM 
-    app.get('/forum', function(req, res) {
-      res.render('forum.ejs');
-    })
-
-    app.get('/forumcat', function(req, res) {
-      res.render('forumcat.ejs')
-    })
-
-    app.get('/forumtop', function(req, res) {
-      res.render('forumtop.ejs')
-    })
-
-    app.get('/forumpost', function(req, res) {
-      res.render('forumpost.ejs')
-    })
-
-    app.get('/knowledge.ejs', isLoggedIn, function(req, res) {
-      res.render('knowledge.ejs')
-    })
-
-// Discussion Forum Routes ===============================================================
-
-    app.post('/topic', (req, res) => {
-      let time = (new Date()).toLocaleString();
-      db.collection('topics').insertOne({topic: req.body.topic, comment: req.body.comment, upVotes: 0, time,}, (err, result) => {
+    app.get('/forum',  async function(req, res) {
+      let user = req.user
+     await db.collection('users').find().toArray((err, result) => {
         if (err) return console.log(err)
-        console.log(req.body)
-        console.log('saved to database')
-        alert('posted')
-        res.redirect('/forumPost')
+        res.render('forum.ejs', {
+         result: result,
+         user: user
+        });
+      })
+    })
+    //Sub Cats in URL
+    app.get('/forex/:sub', isLoggedIn, async function(req, res) {
+      let page = req.params.sub
+      var title
+      await db.collection('topics').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        var posts = result.filter(post => post.cat === page)
+        
+        if(page === 'pain'){
+          title = "The Pain and Gains"
+        }else if(page === 'strategy'){
+          title = "STRATEGY"
+        }else if(page === 'general'){
+          title = 'GENERAL'
+        }else if(page === 'predictions'){
+          title = 'PREDICTIONS'
+        }
+        res.render('forumcat.ejs', {
+          cat: page,
+          title: title,
+          posts: posts,
+        })
       })
     })
 
+    //display the topic 
+    app.get('/forumtop/:postId', isLoggedIn, function(req,res) {
+      let postId = ObjectId(req.params.postId)
+      db.collection('topics').find({_id: postId}).toArray((err, result) => {
+        db.collection('comment').find({post: postId}).toArray((err, mainResult) => {
+          if (err) return console.log(err)
+          console.log(mainResult)
+          res.render('forumtop.ejs', {
+            user: req.user,
+            result: result[0],
+            mainResult: mainResult
+          })
+        })
+      })
+    });
 
+    //Post with category
+    app.get('/forumpost/:cat', isLoggedIn, function (req, res) {
+      let cat = req.params.cat
+      res.render('forumpost.ejs', {
+        cat: cat
+      })
+    })
+// Discussion Forum Routes ===============================================================
+//Post a topic in cat
+    app.post('/topic/:cat', upload.single('file-to-upload'), (req, res) => {
+      let user = req.user
+      let cat = req.params.cat
+      let time = (new Date()).toLocaleString();
+      db.collection('topics').insertOne({
+        topic: req.body.topic, 
+        comment: req.body.comment, 
+        likes: 0, 
+        liked: false, 
+        time, 
+        cat, 
+        tag: req.body.tag, 
+        img: '/images/uploads/' + req.file.filename,
+        postedBy: user.local.email}, 
+        (err, result) => {
+          if (err) return console.log(err)
+        console.log('saved to database')
+        res.render('forum.ejs')
+      })
+    })
 
+    app.post('/comment/:postId', (req,res) => {
+      let user = req.user;
+      let time = (new Date()).toLocaleString();
+      let postId = req.params.postId
+      console.log('hi im a',req.body)
+      db.collection('comments').insertOne({
+        commentBy: user.local.email,
+        comment: req.body.comment,
+        likes: 0,
+        liked: false,
+        time,
+        post: postId
+      }, (err, result) => {
+        if (err) return console.log(err)
+        res.redirect('back')
+      })
+    })
 
-    app.put('/messages', (req, res) => {
-      db.collection('messages')
-      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    app.put('/topicLikes', (req, res) => {
+      db.collection('topics')
+      .findOneAndUpdate({_id: ObjectId(req.body._id)}, {
+        $inc: {
+          likes: 1,
+        },
         $set: {
-          thumbUp:req.body.thumbUp + 1
+          liked: true,
         }
       }, {
         sort: {_id: -1},
@@ -78,13 +160,15 @@ module.exports = function(app, passport, db, multer) {
       }, (err, result) => {
         if (err) return res.send(err)
         res.send(result)
+        console.log(result); //Delete later
       })
     })
 
-    app.delete('/messages', (req, res) => {
-      db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+
+    app.delete('/topicDelete', (req, res) => {
+      db.collection('topics').findOneAndDelete({_id: ObjectId(req.body.id)}, (err, result) => {
         if (err) return res.send(500, err)
-        res.send('Message deleted!')
+        res.send(result)
       })
     })
 
@@ -101,7 +185,7 @@ module.exports = function(app, passport, db, multer) {
 
         // process the login form
         app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/forum', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
@@ -114,7 +198,7 @@ module.exports = function(app, passport, db, multer) {
 
         // process the signup form
         app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/forum', // redirect to the secure profile section
             failureRedirect : '/signup', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
@@ -143,5 +227,5 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
 
-    res.redirect('/');
+    res.redirect('forum.ejs');
 }
